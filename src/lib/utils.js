a866d0e3fd22bd704ca3117dfb0a35d10cbbe96c
Gabrielricco12 <gabrielricco2023@gmail.com>
2025-08-15 14:00:46 -0300

    utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,10 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Combina classes do Tailwind com clsx + tailwind-merge
+ * Exemplo: cn("p-4", isActive && "bg-blue-500")
+ */
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
